6d47bc25ea900eb91709791b2228f8339c778361
Gustavo Fernandes <contato.gustavofernandes@hotmail.com>
2021-10-26 10:13:36 -0300

    checkpoint

--- contracts/Provider.sol
+++ contracts/Provider.sol
@@ -1,10 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
-contract Provider {
-    address admin;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-    constructor() {}
+contract Provider is ERC20 {
+    address admin;
+    IERC20 dai;
+
+    constructor(address _dai) ERC20("Liquidity Provider", "LP") {
+        admin = msg.sender;
+        dai = IERC20(_dai);
+    }
 
     //100 basis points = 1%
     function calculateFee(uint256 amount) external pure returns (uint256) {
---
+++ migrations/2_deploy.js
@@ -0,0 +1,10 @@
+const Dai = artifacts.require("Dai");
+const Provider = artifacts.require("Provider");
+
+module.exports = async (deployer) => {
+  await deployer.deploy(Dai);
+  const dai = await Dai.deployed();
+
+  await deployer.deploy(Provider, dai.address);
+  const provider = await Provider.deployed();
+};
